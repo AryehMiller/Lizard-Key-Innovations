a42fab5b35bf0984c05f8a909dedb6104aacbc8a
Ari Miller <42010594+AryehMiller@users.noreply.github.com>
2021-02-28 11:56:24 -0600

    Update Phylo.D.All.R

--- D-Statistic/Phylo.D.All.R
+++ D-Statistic/Phylo.D.All.R
@@ -12,8 +12,8 @@ library(caper)
 
 ## Read in datasets
 
-sse_dataset_strict_toepads <- read.csv("FullStrict_TraitAnalysis.csv")
-sse_dataset_relaxed_toepads <- read.csv("FullRelaxed_TraitAnalysis.csv")
+Full_Strict_Dataset <- read.csv("FullStrict_TraitAnalysis.csv")
+Full_Relaxed_Dataset <- read.csv("FullRelaxed_TraitAnalysis.csv")
 
 ## Read in phylogeny
 
@@ -21,25 +21,25 @@ FullTree <- read.nexus(file = "FullTree.tre")
 
 ## Full D-Stat Toepads
 
-Concatenated.Full <- sse_dataset_strict_toepads$Concatenated
+Concatenated.Full <- Full_Strict_Dataset$Concatenated
 
 ## Standard D-Stat Toepads
 
-Toepads.Full <- sse_dataset_strict_toepads$toepads
+Toepads.Full <- Full_Strict_Dataset$toepads
 Full.Toepads.Combo <- data.frame(Concatenated.Full, Toepads.Full)
 Full.Toepads.PhyloD <- phylo.d(Full.Toepads.Combo,FullTree, names.col = Concatenated.Full, binvar = Toepads.Full)
 Full.Toepads.PhyloD
 
 ## Full Strict Arboreality D-Stat
 
-Full.Arboreal.Strict<- sse_dataset_strict_toepads$strict.arboreal
+Full.Arboreal.Strict<- Full_Strict_Dataset$strict.arboreal
 Full.Strict.Arboreal.Combo <- data.frame(Concatenated.Full, Full.Arboreal.Strict)
 Full.Strict.Arboreal.PhyloD <- phylo.d(Full.Strict.Arboreal.Combo, FullTree, names.col = Concatenated.Full, binvar=Full.Arboreal.Strict)
 Full.Strict.Arboreal.PhyloD
 
 ## Full Relaxed Arboreality D-Stat
 
-Full.Arboreal.Relaxed <- sse_dataset_relaxed_toepads$relaxed.arboreal
+Full.Arboreal.Relaxed <- Full_Relaxed_Dataset$relaxed.arboreal
 Full.Relaxed.Arboreal.Combo <- data.frame(Concatenated.Full, Full.Arboreal.Relaxed)
 Full.Relaxed.Arboreal.PhyloD <- phylo.d(Full.Relaxed.Arboreal.Combo, FullTree, names.col = Concatenated.Full, binvar=Full.Arboreal.Relaxed)
 Full.Relaxed.Arboreal.PhyloD
@@ -48,8 +48,8 @@ Full.Relaxed.Arboreal.PhyloD
 
 ## Read in dataset
 
-sse_dataset_Standard_Strict_toepads<-read.csv("StandardStrict_TraitAnalysis.csv")
-sse_dataset_Standard_Relaxed_toepads<-read.csv("StandardRelaxed_TraitAnalysis.csv")
+Standard_Strict_Dataset<-read.csv("StandardStrict_TraitAnalysis.csv")
+Standard_Relaxed_Dataset<-read.csv("StandardRelaxed_TraitAnalysis.csv")
 
 ## Read in tree
 
@@ -57,26 +57,25 @@ StandardTree <- read.nexus(file = "StandardTree.tre")
 
 ## Isolate species names
 
-Concatenated.Standard <- sse_dataset_Standard_Strict_toepads$Concatenated
+Concatenated.Standard <- Standard_Strict_Dataset$Concatenated
 
 ## Standard D-Stat Toepads
 
-Toepads.Standard <- sse_dataset_Standard_Strict_toepads$toepads
+Toepads.Standard <- Standard_Strict_Dataset$toepads
 Toepads.Standard.Combo <- data.frame(Concatenated.Standard, Toepads.Standard)
 Toepads.Standard.PhyloD <- phylo.d(Toepads.Standard.Combo,StandardTree, names.col = Concatenated.Standard, binvar = Toepads.Standard)
 Toepads.Standard.PhyloD
 
 ## Standard Strict Arboreality D-Stat
 
-Standard.Arboreal.Strict<- sse_dataset_Standard_Strict_toepads$strict.arboreal
+Standard.Arboreal.Strict<- Standard_Strict_Dataset$strict.arboreal
 Standard.Strict.Arboreal.Combo <- data.frame(Concatenated.Standard, Standard.Arboreal.Strict)
 Standard.Strict.Arboreal.PhyloD <- phylo.d(Standard.Strict.Arboreal.Combo, StandardTree, names.col = Concatenated.Standard, binvar=Standard.Arboreal.Strict)
 Standard.Strict.Arboreal.PhyloD
 
 ## Standard Relaxed Arboreality D-Stat
 
-Standard.Arboreal.Relaxed <- sse_dataset_Standard_Relaxed_toepads$relaxed.arboreal
+Standard.Arboreal.Relaxed <- Standard_Relaxed_Dataset$relaxed.arboreal
 Standard.Relaxed.Arboreal.Combo <- data.frame(Concatenated.Standard, Standard.Arboreal.Relaxed)
 Standard.Relaxed.Arboreal.PhyloD <- phylo.d(Standard.Relaxed.Arboreal.Combo, StandardTree, names.col = Concatenated.Standard, binvar=Standard.Arboreal.Relaxed)
 Standard.Relaxed.Arboreal.PhyloD
-
