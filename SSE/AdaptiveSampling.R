80b2da3d263a4bd95b51f4a6e3d285aead36c80c
Ari Miller <42010594+AryehMiller@users.noreply.github.com>
2021-03-30 19:15:28 -0500

    Update AdaptiveSampling.R

--- SSE/AdaptiveSampling.R
+++ SSE/AdaptiveSampling.R
@@ -6,16 +6,11 @@ library(ggplot2)
 library(reshape2)
 library(ggpubr)
 
-## For each dataset, conduct the adaptive sampling like such:
-AdaptiveSampling.object <- SupportRegionMuHiSSE(muhisse.obj = x, n.points = 10000, scale.int = 0.1)
+## Conduct adaptive sampling using hisse::SupportRegionMuHiSSE
 Strict.Full.MuH.CID7.AdaptiveSampling <- SupportRegionMuHiSSE(muhisse.obj = Strict.MuH.CID7, n.points = 10000)
 Strict.Standard.MuH.CID5.AdaptiveSampling <- SupportRegionMuHiSSE(muhisse.obj = Strict.Standard.MuH.CID5, n.points = 10000)
 Relaxed.Standard.MuH.CID8.AdaptiveSampling <- SupportRegionMuHiSSE(muhisse.obj = Relaxed.Standard.MuH.CID8, n.points = 10000, scale.int = 0.05)
-
-
-
-## x = an object of class muhisse.fit that contains the MLE from a model run-- this analysis should conducted 4x in total, once for each dataset.
-## NOTE: scale.int was changed to 0.05 for the Standard Relaxed dataset owing to efficiency issues.
+Relaxed.Full.MuH.CID8.AdaptiveSampling <- SupportRegionMuHiSSE(muhisse.obj = Relaxed.MuH.CID8, n.points = 10000)
 
 ## Relaxed Standard
 Relaxed.Standard.MuH.CID8.ci <- as.data.frame(Relaxed.Standard.MuH.CID8.AdaptiveSampling$ci)
@@ -56,22 +51,22 @@ Relaxed.Standard.MuH.CID8.ci.Rates.11.10 <- Relaxed.Standard.MuH.CID8.ci.Rates.q
 ## Collate all into a new dataframe for plotting
 
 RelaxedStandard.qrates.adaptivesampling.ci.q <- cbind(Relaxed.Standard.MuH.CID8.ci.Rates.00.10, 
-                                                 Relaxed.Standard.MuH.CID8.ci.Rates.10.00, 
-                                                 Relaxed.Standard.MuH.CID8.ci.Rates.00.01, 
-                                                 Relaxed.Standard.MuH.CID8.ci.Rates.01.00, 
-                                                 Relaxed.Standard.MuH.CID8.ci.Rates.01.11, 
-                                                 Relaxed.Standard.MuH.CID8.ci.Rates.11.01, 
-                                                 Relaxed.Standard.MuH.CID8.ci.Rates.10.11, 
-                                                 Relaxed.Standard.MuH.CID8.ci.Rates.11.10)
+                                                      Relaxed.Standard.MuH.CID8.ci.Rates.10.00, 
+                                                      Relaxed.Standard.MuH.CID8.ci.Rates.00.01, 
+                                                      Relaxed.Standard.MuH.CID8.ci.Rates.01.00, 
+                                                      Relaxed.Standard.MuH.CID8.ci.Rates.01.11, 
+                                                      Relaxed.Standard.MuH.CID8.ci.Rates.11.01, 
+                                                      Relaxed.Standard.MuH.CID8.ci.Rates.10.11, 
+                                                      Relaxed.Standard.MuH.CID8.ci.Rates.11.10)
 
 
 RelaxedStandard.qrates.adaptivesampling.ci.q <- select(RelaxedStandard.qrates.adaptivesampling.ci.q,contains("A"))
 
 RelaxedStandard.qrates.adaptivesampling.ci.q <-as.data.frame(RelaxedStandard.qrates.adaptivesampling.ci.q, row.names = c("0%",
-                                                                                                               "25%",
-                                                                                                               "50%",
-                                                                                                               "75%",
-                                                                                                               "100%"))
+                                                                                                                         "25%",
+                                                                                                                         "50%",
+                                                                                                                         "75%",
+                                                                                                                         "100%"))
 
 RelaxedStandard.qrates.adaptivesampling.ci.q <- tibble::rownames_to_column(RelaxedStandard.qrates.adaptivesampling.ci.q, "Region")
 #write.csv(RelaxedStandard.qrates.adaptivesampling.ci.q,"MuH_CID8_RelaxedStandard.qrates.adaptivesampling.ci.q.csv")
@@ -79,16 +74,16 @@ RelaxedStandard.qrates.adaptivesampling.ci.q.Melted <- melt((RelaxedStandard.qra
 RelaxedStandard.qrates.adaptivesampling.ci.q.Melted$Region <- factor(RelaxedStandard.qrates.adaptivesampling.ci.q.Melted$Region, levels = c("0%", "25%", "50%","75%","100%"))
 
 Relaxed.Standard.level.order <- factor(RelaxedStandard.qrates.adaptivesampling.ci.q.Melted$variable, level = c('q00A_01A',
-                                                                                                     'q11A_10A',
-                                                                                                     'q10A_11A',
-                                                                                                     'q00A_10A',
-                                                                                                     'q01A_00A',
-                                                                                                     'q01A_11A',
-                                                                                                     'q11A_01A',
-                                                                                                     'q10A_00A'))
+                                                                                                               'q11A_10A',
+                                                                                                               'q10A_11A',
+                                                                                                               'q00A_10A',
+                                                                                                               'q01A_00A',
+                                                                                                               'q01A_11A',
+                                                                                                               'q11A_01A',
+                                                                                                               'q10A_00A'))
 
 RelaxedStandard.qrates.adaptivesampling.ci.q.Melted.Jitter <- ggplot(RelaxedStandard.qrates.adaptivesampling.ci.q.Melted, aes(x=Relaxed.Standard.level.order, y = value))+ geom_jitter(aes(color = Region), 
-                                                                                                                                                                        position = position_jitter(0), size = 3) +
+                                                                                                                                                                                       position = position_jitter(0), size = 3) +
   xlab("") + ylab("Transition Rate")+ggtitle("Relaxed Standard Adaptive Sampling (MuH CID8)")+ scale_color_manual(values=c( "#EFF3FF", "#BDD7E7" ,"#6BAED6", "#3182BD", "#08519C"))+theme_classic()+coord_flip()+ scale_fill_discrete(breaks=c("0%","25%","50%","75%","100%"))
 
 
@@ -187,15 +182,15 @@ StrictStandard.qrates.adaptivesampling.ci.q <- tibble::rownames_to_column(Strict
 StrictStandard.qrates.adaptivesampling.ci.q.Melted <- melt((StrictStandard.qrates.adaptivesampling.ci.q))
 StrictStandard.qrates.adaptivesampling.ci.q.Melted$Region <- factor(StrictStandard.qrates.adaptivesampling.ci.q.Melted$Region, levels = c("0%", "25%", "50%","75%","100%"))
 Strict.Standard.level.order <- factor(StrictStandard.qrates.adaptivesampling.ci.q.Melted$variable, level = c('q00A_01A',
-                                                                                                       'q11A_10A',
-                                                                                                       'q10A_11A',
-                                                                                                       'q00A_10A',
-                                                                                                       'q01A_00A',
-                                                                                                       'q01A_11A',
-                                                                                                       'q11A_01A',
-                                                                                                       'q10A_00A'))
+                                                                                                             'q11A_10A',
+                                                                                                             'q10A_11A',
+                                                                                                             'q00A_10A',
+                                                                                                             'q01A_00A',
+                                                                                                             'q01A_11A',
+                                                                                                             'q11A_01A',
+                                                                                                             'q10A_00A'))
 StrictStandard.qrates.adaptivesampling.ci.q.Melted.Jitter <- ggplot(StrictStandard.qrates.adaptivesampling.ci.q.Melted, aes(x = Strict.Standard.level.order,y=value))+ geom_jitter(aes(color = Region), 
-                                                                                                                                                                                  position = position_jitter(0), size = 3) +
+                                                                                                                                                                                   position = position_jitter(0), size = 3) +
   xlab("") + ylab("Transition Rate")+ggtitle("Strict Standard Adaptive Sampling (MuH CID5)")+ scale_color_manual(values=c( "#EFF3FF", "#BDD7E7" ,"#6BAED6", "#3182BD", "#08519C"))+theme_classic()+coord_flip()+ scale_fill_discrete(breaks=c("0%","25%","50%","75%","100%"))
 
 #brewer.pal(n = 5, name = "Blues")
@@ -203,7 +198,7 @@ StrictStandard.qrates.adaptivesampling.ci.q.Melted.Jitter <- ggplot(StrictStanda
 #dev.size("in") #[1] 10.77778  5.87500
 
 ## Relaxed Full
-Relaxed.Full.MuH.CID8.AdaptiveSampling <- Relaxed.MuH.CID8.CI
+Relaxed.Full.MuH.CID8.AdaptiveSampling <- Relaxed.Full.MuH.CID8.AdaptiveSampling
 Relaxed.Full.MuH.CID8.ci <- as.data.frame(Relaxed.Full.MuH.CID8.AdaptiveSampling$ci)
 Relaxed.Full.MuH.CID8.ci <- tibble::rownames_to_column(Relaxed.Full.MuH.CID8.ci, "Region")
 
@@ -242,22 +237,22 @@ Relaxed.Full.MuH.CID8.ci.Rates.11.10 <- Relaxed.Full.MuH.CID8.ci.Rates.q10q11 %>
 ## Collate all into a new dataframe for plotting
 
 RelaxedFull.qrates.adaptivesampling.ci.q <- cbind(Relaxed.Full.MuH.CID8.ci.Rates.00.10, 
-                                                     Relaxed.Full.MuH.CID8.ci.Rates.10.00, 
-                                                     Relaxed.Full.MuH.CID8.ci.Rates.00.01, 
-                                                     Relaxed.Full.MuH.CID8.ci.Rates.01.00, 
-                                                     Relaxed.Full.MuH.CID8.ci.Rates.01.11, 
-                                                     Relaxed.Full.MuH.CID8.ci.Rates.11.01, 
-                                                     Relaxed.Full.MuH.CID8.ci.Rates.10.11, 
-                                                     Relaxed.Full.MuH.CID8.ci.Rates.11.10)
+                                                  Relaxed.Full.MuH.CID8.ci.Rates.10.00, 
+                                                  Relaxed.Full.MuH.CID8.ci.Rates.00.01, 
+                                                  Relaxed.Full.MuH.CID8.ci.Rates.01.00, 
+                                                  Relaxed.Full.MuH.CID8.ci.Rates.01.11, 
+                                                  Relaxed.Full.MuH.CID8.ci.Rates.11.01, 
+                                                  Relaxed.Full.MuH.CID8.ci.Rates.10.11, 
+                                                  Relaxed.Full.MuH.CID8.ci.Rates.11.10)
 
 
 RelaxedFull.qrates.adaptivesampling.ci.q <- select(RelaxedFull.qrates.adaptivesampling.ci.q,contains("A"))
 
 RelaxedFull.qrates.adaptivesampling.ci.q <-as.data.frame(RelaxedFull.qrates.adaptivesampling.ci.q, row.names = c("0%",
-                                                                                                                       "25%",
-                                                                                                                       "50%",
-                                                                                                                       "75%",
-                                                                                                                       "100%"))
+                                                                                                                 "25%",
+                                                                                                                 "50%",
+                                                                                                                 "75%",
+                                                                                                                 "100%"))
 
 RelaxedFull.qrates.adaptivesampling.ci.q <- tibble::rownames_to_column(RelaxedFull.qrates.adaptivesampling.ci.q, "Region")
 #write.csv(RelaxedFull.qrates.adaptivesampling.ci.q,"MuHCID8.RelaxedFull.qrates.adaptivesampling.ci.q.csv")
@@ -266,17 +261,17 @@ RelaxedFull.qrates.adaptivesampling.ci.q.Melted <- melt((RelaxedFull.qrates.adap
 RelaxedFull.qrates.adaptivesampling.ci.q.Melted$Region <- factor(RelaxedFull.qrates.adaptivesampling.ci.q.Melted$Region, levels = c("0%", "25%", "50%","75%","100%"))
 
 Relaxed.Full.level.order <- factor(RelaxedFull.qrates.adaptivesampling.ci.q.Melted$variable, level = c('q00A_01A',
-                                                                                                     'q11A_10A',
-                                                                                                     'q10A_11A',
-                                                                                                     'q00A_10A',
-                                                                                                     'q01A_00A',
-                                                                                                     'q01A_11A',
-                                                                                                     'q11A_01A',
-                                                                                                     'q10A_00A'))
+                                                                                                       'q11A_10A',
+                                                                                                       'q10A_11A',
+                                                                                                       'q00A_10A',
+                                                                                                       'q01A_00A',
+                                                                                                       'q01A_11A',
+                                                                                                       'q11A_01A',
+                                                                                                       'q10A_00A'))
 
 
 RelaxedFull.qrates.adaptivesampling.ci.q.Melted.Jitter <- ggplot(RelaxedFull.qrates.adaptivesampling.ci.q.Melted, aes(x = Relaxed.Full.level.order, y =value))+ geom_jitter(aes(color = Region), 
-                                                                                                                                                                                  position = position_jitter(0), size = 3) +
+                                                                                                                                                                            position = position_jitter(0), size = 3) +
   xlab("") + ylab("Transition Rate")+ggtitle("Relaxed Full Adaptive Sampling (MuH CID8)")+ scale_color_manual(values=c( "#EFF3FF", "#BDD7E7" ,"#6BAED6", "#3182BD", "#08519C"))+theme_classic()+coord_flip()+ scale_fill_discrete(breaks=c("0%","25%","50%","75%","100%"))
 
 ## Strict Full
@@ -318,22 +313,22 @@ Strict.Full.MuH.CID7.ci.Rates.11.10 <- Strict.Full.MuH.CID7.ci.Rates.q10q11 %>% 
 ## Collate all into a new dataframe for plotting
 
 StrictFull.qrates.adaptivesampling.ci.q <- cbind(Strict.Full.MuH.CID7.ci.Rates.00.10, 
-                                                  Strict.Full.MuH.CID7.ci.Rates.10.00, 
-                                                  Strict.Full.MuH.CID7.ci.Rates.00.01, 
-                                                  Strict.Full.MuH.CID7.ci.Rates.01.00, 
-                                                  Strict.Full.MuH.CID7.ci.Rates.01.11, 
-                                                  Strict.Full.MuH.CID7.ci.Rates.11.01, 
-                                                  Strict.Full.MuH.CID7.ci.Rates.10.11, 
-                                                  Strict.Full.MuH.CID7.ci.Rates.11.10)
+                                                 Strict.Full.MuH.CID7.ci.Rates.10.00, 
+                                                 Strict.Full.MuH.CID7.ci.Rates.00.01, 
+                                                 Strict.Full.MuH.CID7.ci.Rates.01.00, 
+                                                 Strict.Full.MuH.CID7.ci.Rates.01.11, 
+                                                 Strict.Full.MuH.CID7.ci.Rates.11.01, 
+                                                 Strict.Full.MuH.CID7.ci.Rates.10.11, 
+                                                 Strict.Full.MuH.CID7.ci.Rates.11.10)
 
 
 StrictFull.qrates.adaptivesampling.ci.q <- select(StrictFull.qrates.adaptivesampling.ci.q,contains("A"))
 
 StrictFull.qrates.adaptivesampling.ci.q <-as.data.frame(StrictFull.qrates.adaptivesampling.ci.q, row.names = c("0%",
-                                                                                                                 "25%",
-                                                                                                                 "50%",
-                                                                                                                 "75%",
-                                                                                                                 "100%"))
+                                                                                                               "25%",
+                                                                                                               "50%",
+                                                                                                               "75%",
+                                                                                                               "100%"))
 
 StrictFull.qrates.adaptivesampling.ci.q <- tibble::rownames_to_column(StrictFull.qrates.adaptivesampling.ci.q, "Region")
 #write.csv(StrictFull.qrates.adaptivesampling.ci.q,"MuHCID8.StrictFull.qrates.adaptivesampling.ci.q.csv")
@@ -361,4 +356,3 @@ AdaptiveSamplingAll.Final <- ggarrange(
 
 #For mean + sd
 # + stat_summary(aes(color = value), size = 0.3,fun.data="mean_sdl", alpha=0.7,color="gray70", fun.args = list(mult=1))
-
